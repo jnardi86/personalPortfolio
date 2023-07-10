a265ad872f3f280571d050beec99c9b0ae83dc29
jnardi86 <julian.nardi@gmail.com>
2023-07-10 19:28:10 -0300

    First commit add Navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar'
+import Banner from './components/Banner';
+
+function App() {
+  
+  return (
+    <>
+      <NavBar/>
+      <Banner/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const Banner = () => {
+  return (
+    <section className='banner' id='home'>
+        <Container>
+            
+        </Container>
+    </section>
+  )
+}
+
+export default Banner
